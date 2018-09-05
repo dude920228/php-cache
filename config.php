d45b2ac33e8e995e4e5e2b00ca83c6a13e25bd11
Dudás Krisztián <dude920228@gmail.com>
2018-09-05 10:25:28 +0200

    Modified port in config.php

--- config.php
+++ config.php
@@ -15,10 +15,10 @@ use PhpCache\Storage\MaintainerFactory;
 return [
     'memoryLimit' => 1024, //To be used in future feature: Backing up data to file, after Bucket size exceeds memory limit
     'ip'          => '0.0.0.0',
-    'port'        => 1234,
+    'port'        => 9000,
     'bufferSize'  => 256,
     'ttl'         => 3600, // Time To Live -> defines how many seconds the cache should persist an entry (Default 3600)
-    'backupTime'  => 3600, // Creates a file storage backup every $backupTime seconds (Default 3600)
+    'backupTime'  => 1800, // Creates a file storage backup every $backupTime seconds
     'backupDir'   => __DIR__.'/.backup',
     'factories'   => [
         CacheServer::class    => CacheServerFactory::class,
