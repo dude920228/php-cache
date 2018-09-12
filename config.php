8b5e922d96f0fd86ea12e34da2f8485393493f46
dude920228 <dude920228@gmail.com>
2018-09-12 08:42:29 +0000

    Apply fixes from StyleCI

--- config.php
+++ config.php
@@ -15,20 +15,20 @@ use PhpCache\Storage\MaintainerFactory;
 return [
     'config' => [
         'memoryLimit' => 1024, //To be used in future feature: Backing up data to file, after Bucket size exceeds memory limit
-        'ip' => '0.0.0.0',
-        'port' => 9000,
-        'bufferSize' => 256,
-        'ttl' => 3600, // Time To Live -> defines how many seconds the cache should persist an entry (Default 3600)
-        'backupTime' => 1800, // Creates a file storage backup every $backupTime seconds
-        'backupDir' => __DIR__ . '/.backup',
+        'ip'          => '0.0.0.0',
+        'port'        => 9000,
+        'bufferSize'  => 256,
+        'ttl'         => 3600, // Time To Live -> defines how many seconds the cache should persist an entry (Default 3600)
+        'backupTime'  => 1800, // Creates a file storage backup every $backupTime seconds
+        'backupDir'   => __DIR__.'/.backup',
     ],
     'services' => [
         'factories' => [
-            CacheServer::class => CacheServerFactory::class,
+            CacheServer::class    => CacheServerFactory::class,
             CacheIOHandler::class => CacheIOHandlerFactory::class,
-            CacheClient::class => CacheClientFactory::class,
-            Maintainer::class => MaintainerFactory::class,
-            Bucket::class => BucketFactory::class,
+            CacheClient::class    => CacheClientFactory::class,
+            Maintainer::class     => MaintainerFactory::class,
+            Bucket::class         => BucketFactory::class,
         ],
         'invokables' => [
             ActionHandler::class => ActionHandler::class,
