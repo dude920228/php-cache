ba02152af52e9461af3fe3c86ae171b904d79065
Dudás Krisztián <dude920228@gmail.com>
2018-09-14 15:58:46 +0200

    Class use change

--- testServer.php
+++ testServer.php
@@ -5,10 +5,11 @@ ini_set('error_log', '/var/log/php-cache.log');
 require_once 'vendor/autoload.php';
 
 use PhpCache\CacheServer\CacheServer;
+use PhpCache\ServiceManager\ConfigAggregator;
 use PhpCache\ServiceManager\ServiceManager;
 
 $config = include_once 'config.php';
-$configAggregator = new \PhpCache\ServiceManager\ConfigAggregator();
+$configAggregator = new ConfigAggregator();
 $configAggregator->addConfig($config);
 $serviceManager = new ServiceManager($configAggregator->getMergedConfig());
 $server = $serviceManager->get(CacheServer::class);
